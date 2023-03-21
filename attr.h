a4372e0434470da98197a7f20039e234495308bf
Arnav Gupta <arnavg20@iitk.ac.in>
2023-03-21 19:39:11 +0530

    updated = and == for attr

--- attr.h
+++ attr.h
@@ -23,19 +23,40 @@ public:
 	vector<string> params;
 	vector<string> threeAC;
 	
-	bool operator=(attr const& obj){
+	bool operator==(attr const& obj){
 		if(nodeno!=obj.nodeno) return false;
 		if(type!=obj.type) return false;
+		if(addrName!=obj.addrName) return false;
+		if(addrConst!=obj.addrConst) return false;
 
 		if(params.size()!=(obj.params).size()) return false;
 		
-		for(int i=0;i<params.size();i++){
+		for(int i=0;i<obj.params.size();i++){
 			if(params[i]!=(obj.params)[i]) return false;
 		}
 		for(int i=0;i<threeAC.size();i++){
 			if(threeAC[i]!=(obj.threeAC)[i]) return false;
 		}
 		return true;
+	} 
+
+	void operator=(attr const& obj){
+		nodeno = obj.nodeno;
+		type = obj.type;
+		addrName = obj.addrName;
+		addrConst = obj.addrConst;
+
+		params.clear();
+		
+		for(int i=0;i<obj.params.size();i++){
+			params.push_back((obj.params)[i]);
+		}
+
+		threeAC.clear();
+
+		for(int i=0;i<obj.threeAC.size();i++){
+			threeAC.push_back((obj.threeAC)[i]);
+		}
 	} 
 };
 #endif
