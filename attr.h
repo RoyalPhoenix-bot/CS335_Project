ae666307a36625af6e72f466571f5208db8bd787
Arnav Gupta <arnavg20@iitk.ac.in>
2023-03-21 22:44:07 +0530

    << operator for attr

--- attr.h
+++ attr.h
@@ -30,71 +30,80 @@ public:
 		addrConst=0;
 	}
 
-	bool operator==(attr const& obj){
-		if(nodeno!=obj.nodeno) return false;
-		if(type!=obj.type) return false;
-		if(addrName!=obj.addrName) return false;
-		if(addrConst!=obj.addrConst) return false;
+	bool operator==(attr const& other){
+		if(nodeno!=other.nodeno) return false;
+		if(type!=other.type) return false;
+		if(addrName!=other.addrName) return false;
+		if(addrConst!=other.addrConst) return false;
 
-		if(params.size()!=(obj.params).size()) return false;
-		if(threeAC.size()!=(obj.threeAC).size()) return false;
+		if(params.size()!=(other.params).size()) return false;
+		if(threeAC.size()!=(other.threeAC).size()) return false;
 		
-		for(int i=0;i<obj.params.size();i++){
-			if(params[i]!=(obj.params)[i]) return false;
+		for(int i=0;i<other.params.size();i++){
+			if(params[i]!=(other.params)[i]) return false;
 		}
-		for(int i=0;i<obj.threeAC.size();i++){
-			if(threeAC[i]!=(obj.threeAC)[i]) return false;
+		for(int i=0;i<other.threeAC.size();i++){
+			if(threeAC[i]!=(other.threeAC)[i]) return false;
 		}
 		return true;
 	} 
 
-	void operator=(attr const& obj){
+	void operator=(attr const& other){
 
-		type = obj.type;
-		addrName = obj.addrName;
-		addrConst = obj.addrConst;
+		type = other.type;
+		addrName = other.addrName;
+		addrConst = other.addrConst;
 
 		params.clear();
 		
-		for(int i=0;i<obj.params.size();i++){
-			params.push_back((obj.params)[i]);
+		for(int i=0;i<other.params.size();i++){
+			params.push_back((other.params)[i]);
 		}
 
 		threeAC.clear();
 
-		for(int i=0;i<obj.threeAC.size();i++){
-			threeAC.push_back((obj.threeAC)[i]);
+		for(int i=0;i<other.threeAC.size();i++){
+			threeAC.push_back((other.threeAC)[i]);
 		}
 	} 
 
 
-	attr operator+(attr const& obj){
+	attr operator+(attr const& other){
 		attr res;
 
-		res.type = obj.type;//should be ensured by type checking
+		res.type = other.type;//should be ensured by type checking
 		
-		res.addrName = obj.addrName;//not useful for +
-		res.addrConst = obj.addrConst;//not useful for +
+		res.addrName = other.addrName;//not useful for +
+		res.addrConst = other.addrConst;//not useful for +
 
 		for(int i=0;i<params.size();i++){
 			res.params.push_back(params[i]);
 		}
 
-		for(int i=0;i<obj.params.size();i++){
-			res.params.push_back((obj.params)[i]);
+		for(int i=0;i<other.params.size();i++){
+			res.params.push_back((other.params)[i]);
 		}
 
 		for(int i=0;i<threeAC.size();i++){
 			res.threeAC.push_back(threeAC[i]);
 		}
 
-		for(int i=0;i<obj.threeAC.size();i++){
-			res.threeAC.push_back((obj.threeAC)[i]);
+		for(int i=0;i<other.threeAC.size();i++){
+			res.threeAC.push_back((other.threeAC)[i]);
 		}
 
 		return res;
 	} 
 	
 };
+
+inline ostream& operator<<(ostream &os, const attr& other){
+		
+	for(int i=0;i<other.threeAC.size();i++){
+		os<<(other.threeAC)[i]<<endl;
+	}
+
+	return os;
+}
 
 #endif
