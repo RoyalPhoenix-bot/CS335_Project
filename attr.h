ec32d2976c94173eea670627e213b0adecf2c00a
mandar7-git <mandarwayal762@gmail.com>
2023-03-24 11:41:57 +0530

    attr and treeTraversal files also commited

--- attr.h
+++ attr.h
@@ -12,10 +12,18 @@ enum varTypes{
 	nullType
 };
 
-
 typedef struct Attributes{
+	string type;
+	string name;
+	vector<string> otherParams;
+	vector<int> intParams;
+	int num; // to store a numbered attribute (Ex.to get size of array, to store how many vars declared at once i.e int a,b,c)
+} attr1;
+
+typedef struct Attributes2{
 	int nodeno;
 	varTypes type;
 	vector<string> params;
-} attr;
+} attr2;
+
 #endif
