5a98349931666320dd3dd0dec37809cf2823e2fc
Mandar Wayal <82501714+mandar7-git@users.noreply.github.com>
2023-03-25 23:42:59 +0530

    Create sample1.java

---
+++ m2_test_cases/sample1.java
@@ -0,0 +1,29 @@
+package programs;
+
+public class sample2 {
+    public static void main(String[] args) {
+        int rows = 4;
+        int columns = 3;
+        int depth = 5;
+ 
+        int[][][] array = new int[rows][columns][depth];
+ 
+        int value = 1;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                for (int k = 0; k < depth; k++) {
+                    array[i][j][k] = value;
+                    value++;
+                }
+            }
+        }
+ 
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                for (int k = 0; k < depth; k++) {
+                    value++;
+                }
+            }
+        }
+    }
+}
