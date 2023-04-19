e0413f3418d95d6006dcadd3f681f00111ea4a3e
mandar7-git <mandarwayal762@gmail.com>
2023-04-19 23:34:27 +0530

    Added final TC folder

---
+++ finaltests/sample1.java
@@ -0,0 +1,15 @@
+public class sample1 {
+    
+    public static void main(String[] args /*so that we can compile with javac*/) {
+       
+        int m=4 ;
+
+        for (int i=0;i<10;i++){
+            m=m+1;
+        }
+        return;
+
+    }
+
+
+}
